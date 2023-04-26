ab9192b91c6ea9afa90076346f3072ca344663c9
FERREIRAR <romain.cdesafer@gmail.com>
2023-04-26 10:58:44 +0200

    2604

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -43,25 +43,27 @@ class QuestionController extends AbstractController
     public function questions($theme,$nb_questions, SerializerInterface $serializer): Response
     {
         $themeId = $this->themeRepository->findOneBy(['libelle'=>$theme]);
-        if ($themeId <> null){
-        $questions_theme=$this->questionRepository->findBy(['theme'=>$themeId]);
-       shuffle($questions_theme);
-
-       $questions_array = [];
-       if ($nb_questions >= count($questions_theme)) {
-           $nb_questions = count($questions_theme);
-       }
-       for ($i=$nb_questions;$i>=1;$i=$i-1){
-            array_push($questions_array,$questions_theme[$i-1]);
-        }
-
-        $questions_themeJson = $serializer->serialize( $questions_array,'json',['groups' => 'par_themes']);
-
-        return new Response($questions_themeJson, Response::HTTP_OK, ["content-type" => "application/json"]);
+        if ($themeId == null){
+            return new Response("Thème Introuvable !", 404, ["content-type" => "application/json"]);
 
         } else {
 
-            return new Response("Thème Introuvable !", 404, ["content-type" => "application/json"]);
+            $questions_theme=$this->questionRepository->findBy(['theme'=>$themeId]);
+            shuffle($questions_theme);
+
+            $questions_array = array_slice($questions_theme,0,$nb_questions);
+
+            //$questions_array = [];
+            //if ($nb_questions >= count($questions_theme)) {
+            //    $nb_questions = count($questions_theme);
+            //}
+            //for ($i=$nb_questions;$i>=1;$i=$i-1){
+            //     array_push($questions_array,$questions_theme[$i-1]);
+            // }
+
+            $questions_themeJson = $serializer->serialize( $questions_array,'json',['groups' => 'par_themes']);
+
+            return new Response($questions_themeJson, Response::HTTP_OK, ["content-type" => "application/json"]);
 
         }
 
